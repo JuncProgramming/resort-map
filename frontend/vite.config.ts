983c53886176dc527ea174916f079db8101ac1e5
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2026-03-25 17:12:35 +0100

    Add commands and dependencies to both package.json files, setup vitest config for @ alias importing and enable tailwind in styles.css

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vitest/config'
+
+import react from '@vitejs/plugin-react'
+import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    globals: true
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-});
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
